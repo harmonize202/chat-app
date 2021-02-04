1f119addc82b87aaf68920d479dd5b653a1f9981
harmonize202 <78188377+harmonize202@users.noreply.github.com>
2021-02-04 13:07:00 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # nameカラムが空の場合、エラーを返す(バリデーションされる)為。
+  validates :name, presence: true
+
 end
